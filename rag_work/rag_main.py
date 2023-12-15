42804726ac5882a2c52cea7f8a29ef14464ca5ee
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-12-15 14:51:25 +0500

    ENH: adding some file Rag

---
+++ rag_work/rag_main.py
@@ -0,0 +1,20 @@
+import google.generativeai as genai
+import os
+import os
+from dotenv import load_dotenv
+import requests
+load_dotenv()
+
+
+DB_URI: str = os.getenv("DB_URI")
+API_KEY: str = os.getenv("API_KEY")
+genai.configure(
+    api_key=API_KEY)
+model = genai.GenerativeModel(
+    model_name='gemini-pro')
+
+response = model.generate_content(
+          'Can I use retrieval-augmented generation using makersuit of gemini pro and how?')
+print(response.text)
+    #  The opposite of hot is cold.'
+        
